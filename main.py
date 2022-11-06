bef4b0423cbfeeb7f560666e9499a1170324295a
irynagrape <109566237+irynagrape@users.noreply.github.com>
2022-11-05 20:07:14 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# Python program to convert decimal into other number systems
+
+dec = int(input("Enter a Decimal Number: "))
+
+
+
+#decimal to binary
+
+print(bin(dec), "in Binary.")
+
+
+
+#decimal to octal
+
+print(oct(dec), "in Octal.")
+
+
+
+#decimal to Hexadecimal
+
+print(hex(dec), "in Hexadecimal.")
+
